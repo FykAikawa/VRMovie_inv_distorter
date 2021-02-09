f5eec9000bf9b0238612eb9fb75d5ee25014876c
Aikawa Fuyuka <78573412+FykAikawa@users.noreply.github.com>
2021-02-10 01:21:40 +0900

    Update correcter.py
    simple pincushion distortion may be better.

--- correcter.py
+++ correcter.py
@@ -13,7 +13,7 @@ def inv_frame(frame,width,height,c0,c1,c2):
     revframe=np.zeros((height, width,3),dtype="uint8")
     a=c2
     b=c1
-    c=1+c0
+    c=c0
     if debug:
         for h in range(0,height,60):
             frame[h,:]=254
@@ -26,13 +26,14 @@ def inv_frame(frame,width,height,c0,c1,c2):
     for h in range(height):
         for w in range(width):
             if w < width //2: #left
-                r = ((h-height/2)**2 + (w-width/4)**2)/(480**2+540**2)
-                scale= (c+b*r+a*r*r) #speculated polynominal function
-                revframe[int((h-height/2)*scale)+height//2][int((w-width/4)*scale)+width//4]= frame[int((h-height/2))+height//2][int((w-width/4))+width//4]
-            else: #right
-                r = ((h-height/2)**2 + (w-width*3/4)**2)/(480**2+540**2)
+                r = ((h-height/2)**2 + (w-width/4)**2)/((width//4)**2+(height//2)**2)
                 scale= (c+b*r+a*r*r)
-                revframe[int((h-height/2)*scale)+height//2][int((w-width*3/4)*scale)+width*3//4]= frame[int((h-height/2))+height//2][int((w-width*3/4))+width*3//4]
+                #print(r/math.sqrt(480**2 + 540**2))
+                revframe[h][w]= frame[int((h-height/2)*scale)+height//2][int((w-width/4)*scale)+width//4]
+            else: #right
+                r = ((h-height/2)**2 + (w-width*3/4)**2)/((width//4)**2+(height//2)**2)
+                scale= (c+b*r+a*r*r)
+                revframe[h][w]= frame[int((h-height/2)*scale)+height//2][int((w-width*3/4)*scale)+width*3//4]
     if guide:
         for h in range(0,height,60):
             revframe[h,:]=254
@@ -56,10 +57,10 @@ frame_rate = int(video.get(cv2.CAP_PROP_FPS))
 fmt = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 writer = cv2.VideoWriter('./undistorted.mp4', fmt, frame_rate, wh)
 #heuristic value
-# assert c0+c1+c2 < 1, or this script will crash
-c0= -0.1 
-c1= 0.05
-c2= 0.05
+#assert c0+c1+c2<1 or script will crush
+c0= 1
+c1= -0.1
+c2= -0.1
 
 for i in tqdm(range(frame_count)): #progress bar
     ret, frame = video.read()
